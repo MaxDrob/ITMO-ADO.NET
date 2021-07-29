abea117723dd10964f29937ab43a737f03e3c419
MaxDrob <83185232+MaxDrob@users.noreply.github.com>
2021-07-30 02:17:35 +0300

    adds up

---
+++ DatasetDesigner/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace DatasetDesigner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GetCustomersButton = new System.Windows.Forms.Button();
+            this.CustomersListBox = new System.Windows.Forms.ListBox();
+            this.sqlSelectCommand1 = new System.Data.SqlClient.SqlCommand();
+            this.sqlDataAdapter1 = new System.Data.SqlClient.SqlDataAdapter();
+            this.sqlConnection1 = new System.Data.SqlClient.SqlConnection();
+            this.SuspendLayout();
+            // 
+            // GetCustomersButton
+            // 
+            this.GetCustomersButton.Location = new System.Drawing.Point(1, 4);
+            this.GetCustomersButton.Name = "GetCustomersButton";
+            this.GetCustomersButton.Size = new System.Drawing.Size(151, 23);
+            this.GetCustomersButton.TabIndex = 0;
+            this.GetCustomersButton.Text = "Get Customers";
+            this.GetCustomersButton.UseVisualStyleBackColor = true;
+            this.GetCustomersButton.Click += new System.EventHandler(this.GetCustomersButton_Click);
+            // 
+            // CustomersListBox
+            // 
+            this.CustomersListBox.FormattingEnabled = true;
+            this.CustomersListBox.Location = new System.Drawing.Point(1, 198);
+            this.CustomersListBox.Name = "CustomersListBox";
+            this.CustomersListBox.Size = new System.Drawing.Size(787, 238);
+            this.CustomersListBox.TabIndex = 1;
+            // 
+            // sqlSelectCommand1
+            // 
+            this.sqlSelectCommand1.CommandText = "customers\r\norders";
+            this.sqlSelectCommand1.Connection = this.sqlConnection1;
+            // 
+            // sqlDataAdapter1
+            // 
+            this.sqlDataAdapter1.SelectCommand = this.sqlSelectCommand1;
+            // 
+            // sqlConnection1
+            // 
+            this.sqlConnection1.ConnectionString = "Data Source=DESKTOP-IHB5LCD\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Secur" +
+    "ity=True";
+            this.sqlConnection1.FireInfoMessageEventOnUserErrors = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CustomersListBox);
+            this.Controls.Add(this.GetCustomersButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GetCustomersButton;
+        private System.Windows.Forms.ListBox CustomersListBox;
+        private System.Data.SqlClient.SqlCommand sqlSelectCommand1;
+        private System.Data.SqlClient.SqlConnection sqlConnection1;
+        private System.Data.SqlClient.SqlDataAdapter sqlDataAdapter1;
+    }
+}
+
