168e04690a6da2223694ec3960cd93bab942a831
MaxDrob <83185232+MaxDrob@users.noreply.github.com>
2021-07-30 10:33:57 +0300

    up

--- 1. CreatingDataTable/Form1.Designer.cs
+++ 1. CreatingDataTable/Form1.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace _1.CreatingDataTable
+﻿using System;
+using System.Windows.Forms;
+
+namespace CreatingDataTable
 {
     partial class Form1
     {
@@ -28,13 +31,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.AddRowButton = new System.Windows.Forms.Button();
+            this.TableGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.TableGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AddRowButton
+            // 
+            this.AddRowButton.Location = new System.Drawing.Point(1, 1);
+            this.AddRowButton.Name = "AddRowButton";
+            this.AddRowButton.Size = new System.Drawing.Size(75, 23);
+            this.AddRowButton.TabIndex = 0;
+            this.AddRowButton.Text = "Add Row";
+            this.AddRowButton.UseVisualStyleBackColor = true;
+            this.AddRowButton.Click += new System.EventHandler(this.AddRowButton_Click);
+            // 
+            // TableGrid
+            // 
+            this.TableGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TableGrid.Location = new System.Drawing.Point(1, 201);
+            this.TableGrid.Name = "TableGrid";
+            this.TableGrid.Size = new System.Drawing.Size(800, 255);
+            this.TableGrid.TabIndex = 1;
+            this.TableGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.TableGrid_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TableGrid);
+            this.Controls.Add(this.AddRowButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.TableGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         #endregion
+
+        private System.Windows.Forms.Button AddRowButton;
+        private System.Windows.Forms.DataGridView TableGrid;
     }
 }
 
