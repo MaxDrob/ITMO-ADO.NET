290bb5e31b284e63e4794f1bf56dd6cf1d7f3ed0
MaxDrob <83185232+MaxDrob@users.noreply.github.com>
2021-07-30 15:26:49 +0300

    parts 1, 2 done

---
+++ LoadDataSetXml/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace LoadDataSetXml
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CustomersGrid = new System.Windows.Forms.DataGridView();
+            this.northwindDataSet = new LoadDataSetXml.NorthwindDataSet();
+            this.northwindDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.OrdersGrid = new System.Windows.Forms.DataGridView();
+            this.LoadSchemaButton = new System.Windows.Forms.Button();
+            this.LoadDataButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.CustomersGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrdersGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CustomersGrid
+            // 
+            this.CustomersGrid.AutoGenerateColumns = false;
+            this.CustomersGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.CustomersGrid.DataSource = this.northwindDataSetBindingSource;
+            this.CustomersGrid.Location = new System.Drawing.Point(12, 12);
+            this.CustomersGrid.Name = "CustomersGrid";
+            this.CustomersGrid.Size = new System.Drawing.Size(240, 150);
+            this.CustomersGrid.TabIndex = 0;
+            // 
+            // northwindDataSet
+            // 
+            this.northwindDataSet.DataSetName = "NorthwindDataSet";
+            this.northwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // northwindDataSetBindingSource
+            // 
+            this.northwindDataSetBindingSource.DataSource = this.northwindDataSet;
+            this.northwindDataSetBindingSource.Position = 0;
+            // 
+            // OrdersGrid
+            // 
+            this.OrdersGrid.AutoGenerateColumns = false;
+            this.OrdersGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.OrdersGrid.DataSource = this.northwindDataSetBindingSource;
+            this.OrdersGrid.Location = new System.Drawing.Point(403, 12);
+            this.OrdersGrid.Name = "OrdersGrid";
+            this.OrdersGrid.Size = new System.Drawing.Size(240, 150);
+            this.OrdersGrid.TabIndex = 1;
+            // 
+            // LoadSchemaButton
+            // 
+            this.LoadSchemaButton.Location = new System.Drawing.Point(66, 230);
+            this.LoadSchemaButton.Name = "LoadSchemaButton";
+            this.LoadSchemaButton.Size = new System.Drawing.Size(96, 23);
+            this.LoadSchemaButton.TabIndex = 2;
+            this.LoadSchemaButton.Text = "Load Schema";
+            this.LoadSchemaButton.UseVisualStyleBackColor = true;
+            this.LoadSchemaButton.Click += new System.EventHandler(this.LoadSchemaButton_Click);
+            // 
+            // LoadDataButton
+            // 
+            this.LoadDataButton.Location = new System.Drawing.Point(451, 230);
+            this.LoadDataButton.Name = "LoadDataButton";
+            this.LoadDataButton.Size = new System.Drawing.Size(102, 23);
+            this.LoadDataButton.TabIndex = 3;
+            this.LoadDataButton.Text = "Load Data";
+            this.LoadDataButton.UseVisualStyleBackColor = true;
+            this.LoadDataButton.Click += new System.EventHandler(this.LoadDataButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LoadDataButton);
+            this.Controls.Add(this.LoadSchemaButton);
+            this.Controls.Add(this.OrdersGrid);
+            this.Controls.Add(this.CustomersGrid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.CustomersGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrdersGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView CustomersGrid;
+        private System.Windows.Forms.BindingSource northwindDataSetBindingSource;
+        private NorthwindDataSet northwindDataSet;
+        private System.Windows.Forms.DataGridView OrdersGrid;
+        private System.Windows.Forms.Button LoadSchemaButton;
+        private System.Windows.Forms.Button LoadDataButton;
+    }
+}
+
